6deff6b42649287a8e372bf7ed4857df7a69bc65
Langsrud <oyl@ssb.no>
2019-10-17 16:06:49 +0200

    Laplace noise

---
+++ R/div.R
@@ -0,0 +1,15 @@
+#' Laplace noise
+#'
+#' @param n number of observations
+#' @param b scale parameter(s)
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' Lap(4)
+#' Lap(4, 20)
+#' Lap(4, 1:4)
+Lap <- function(n = 1, b = 1) {
+  rexp(n, 1/b) * sample(c(-1, 1), n, replace = TRUE)
+}
