67d5c83d65c2f255469c3e5c94e9b0a422c2acde
Langsrud <oyl@ssb.no>
2019-10-18 09:46:50 +0200

    Terms2formula: Model formula from vector of terms where intercept is specified as "1"

--- R/div.R
+++ R/div.R
@@ -13,3 +13,28 @@
 Lap <- function(n = 1, b = 1) {
   rexp(n, 1/b) * sample(c(-1, 1), n, replace = TRUE)
 }
+
+
+
+#' Model formula from vector of terms where intercept is specified as \code{"1"}
+#'
+#' @param x Character vector
+#' @param InterceptAs1 Whether to specify intercept as \code{"1"}
+#' @param env Parameter to \code{\link{as.formula}}
+#'
+#' @return formula
+#' @export
+#'
+#' @examples
+#' Terms2formula(c("a", "b:c"))
+#' Terms2formula(c("a", "b:c"), FALSE)
+#' Terms2formula(c("a", "b:c", "1"))
+Terms2formula <- function(x, InterceptAs1 = TRUE, env = parent.frame()) {
+  x1 <- x == "1"
+  if (InterceptAs1) 
+    x <- x[!x1]
+  f <- as.formula(paste("~", paste(x, collapse = " + "), sep = ""), env = env)
+  if (!InterceptAs1 | sum(x1)) 
+    return(f)
+  update(f, ~. - 1)
+}
