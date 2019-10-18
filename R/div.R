a98ef7a2e68a9a09cbae046a4716c16e8cd6b1f0
Langsrud <oyl@ssb.no>
2019-10-18 10:25:46 +0200

    Modify Terms2formula

--- R/div.R
+++ R/div.R
@@ -16,25 +16,35 @@ Lap <- function(n = 1, b = 1) {
 
 
 
-#' Model formula from vector of terms where intercept is specified as \code{"1"}
+#' Model formula from vector of terms where intercept is specified as a string
 #'
 #' @param x Character vector
-#' @param InterceptAs1 Whether to specify intercept as \code{"1"}
+#' @param intercept String to specify intercept
 #' @param env Parameter to \code{\link{as.formula}}
 #'
 #' @return formula
+#' 
+#' @importFrom stats as.formula update
 #' @export
 #'
 #' @examples
 #' Terms2formula(c("a", "b:c"))
-#' Terms2formula(c("a", "b:c"), FALSE)
-#' Terms2formula(c("a", "b:c", "1"))
-Terms2formula <- function(x, InterceptAs1 = TRUE, env = parent.frame()) {
-  x1 <- x == "1"
-  if (InterceptAs1) 
+#' Terms2formula(c("a", "b:c"), NULL)
+#' Terms2formula(c("a", "b:c", "(Intercept)"))
+#' Terms2formula(c("a", "b:c"), "1")
+#' Terms2formula(c("a", "b:c", "1"), "1")
+Terms2formula <- function(x, intercept = "(Intercept)", env = parent.frame()) {
+  if (!is.null(intercept)) {
+    x1 <- x == intercept
     x <- x[!x1]
+    anyx1 <- any(x1)
+  } else {
+    anyx1 <- FALSE
+  }
+  if (!length(x))
+    x <- "1"
   f <- as.formula(paste("~", paste(x, collapse = " + "), sep = ""), env = env)
-  if (!InterceptAs1 | sum(x1)) 
+  if (is.null(intercept) | anyx1) 
     return(f)
   update(f, ~. - 1)
 }
