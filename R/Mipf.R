5d592264a02e53bb82e88d326799f78216f3f02c
Langsrud <oyl@ssb.no>
2021-01-05 19:09:46 +0100

    New version of Mipf utilizing Reduce0exact

--- R/Mipf.R
+++ R/Mipf.R
@@ -1,4 +1,5 @@
-#'Iterative proportional fitting from matrix input
+
+#' Iterative proportional fitting from matrix input
 #' 
 #' The linear equation, \code{z = t(x) \%*\% y}, is (hopefully)  solved for \code{y} by
 #' iterative proportional fitting
@@ -12,10 +13,14 @@
 #' @param yStart a starting estimate of \code{y}
 #' @param eps stopping criterion. Maximum allowed value of \code{max(abs(z - t(x) \%*\% yHat))}
 #' @param tol Another stopping criterion. Maximum absolute difference between two iteration. 
-#' @param reduceBy0 When TRUE, \code{\link{ReduceBy0}}  used within the function 
-#' @param reduceX When TRUE, \code{\link{ReduceBy0}} and  \code{\link{ReduceXspes}} used within the function (iteratively)
-#' @param returnDetails More output when TRUE. Quite similar to \code{\link{ReduceX}}
+#' @param reduceBy0 When TRUE, \code{\link{Reduce0exact}} used within the function 
+#' @param reduceByColSums Parameter to \code{\link{Reduce0exact}} (when TRUE)
+#' @param reduceByLeverage Parameter to \code{\link{Reduce0exact}} (when TRUE)
+#' @param returnDetails More output when TRUE. 
 #' @param y It is possible to set \code{z} to NULL and supply original \code{y} instead  (\code{z = t(x) \%*\% y})
+#' @param startCol Within the algorithm, \code{x} needs to be split into pieces. 
+#'                Vector \code{startCol} is the indices of the first column of each piece (first element is always 1). 
+#'                When \code{NULL} (default), splitting is performed automatically. 
 #' @param ordSplit Whether to order the pieces of the split x according to a rule
 #' @param altSplit Whether to try alternative splitting when ordinary not working properly
 #'        
@@ -25,19 +30,56 @@
 #' @importFrom methods as
 #' @importFrom utils flush.console
 #' @importFrom Matrix drop0
+#' @importFrom SSBtools Reduce0exact 
 #' 
 #' @export
 #' @author Øyvind Langsrud
 #'
 #' @examples
+#' 
 #' data2 <- EasyData("z2")
 #' x <- FormulaSums(data2, ~fylke + kostragr * hovedint - 1)
 #' z <- t(x) %*% data2$ant  # same as FormulaSums(data2, ant~fylke + kostragr * hovedint -1)
 #' yHat <- Mipf(x, z)
 #' 
 #' #############################
-#' # loglin comparison 
+#' # loglm comparison  
 #' #############################
+#' 
+#' if (require(MASS)){
+#' 
+#' # Increase accuracy
+#' yHat <- Mipf(x, z, eps = 1e-04)
+#' 
+#' # Run loglm and store fitted values in a data frame
+#' outLoglm <- loglm(ant ~ fylke + kostragr * hovedint, data2, eps = 1e-04, iter = 100)
+#' dfLoglm <- as.data.frame.table(fitted(outLoglm))
+#' 
+#' # Problem 1: Variable region not in output, but instead the variable .Within.  
+#' # Problem 2: Extra zeros since hierarchy not treated. Impossible combinations in output.
+#' 
+#' # By sorting data, it becomes clear that the fitted values are the same.
+#' max(abs(sort(dfLoglm$Freq, decreasing = TRUE)[1:nrow(data2)] - sort(yHat, decreasing = TRUE)))
+#' 
+#' # Modify so that region is in output. Problem 1 avoided.
+#' x <- FormulaSums(data2, ~region + kostragr * hovedint - 1)
+#' z <- t(x) %*% data2$ant  # same as FormulaSums(data2, ant~fylke + kostragr * hovedint -1)
+#' yHat <- Mipf(x, z, eps = 1e-04)
+#' outLoglm <- loglm(ant ~ region + kostragr * hovedint, data2, eps = 1e-04, iter = 100)
+#' dfLoglm <- as.data.frame.table(fitted(outLoglm))
+#' 
+#' # Now it is possible to merge data
+#' merg <- merge(cbind(data2, yHat), dfLoglm)
+#' 
+#' # Identical output
+#' max(abs(merg$yHat - merg$Freq))
+#' 
+#' }
+#' 
+#' #############################
+#' # loglin comparison  
+#' #############################
+#' 
 #' 
 #' # Generate input data for loglin
 #' n <- 5:9
@@ -73,35 +115,50 @@
 #' 
 #' 
 #' #########################################
-#' #  reduceBy0  and  reduceX examples 
+#' # Examples utilizing Reduce0exact 
 #' #########################################
 #' 
 #' #' z3 <- EasyData("z3")
 #' x <- FormulaSums(z3, ~region + kostragr * hovedint + region * mnd2 + fylke * mnd + 
 #'                      mnd * hovedint + mnd2 * fylke * hovedint - 1)
 #' 
-#' # Reduction by 0, but no iteration improvement. Identical results.
+#' # reduceBy0, but no iteration improvement. Identical results.
 #' t <- 360
 #' y <- z3$ant
 #' y[round((1:t) * 432/t)] <- 0
 #' z <- t(x) %*% y
 #' a1 <- Mipf(x, z, eps = 0.1)
 #' a2 <- Mipf(x, z, reduceBy0 = TRUE, eps = 0.1)
-#' a3 <- Mipf(x, z, reduceX = TRUE, eps = 0.1)
+#' a3 <- Mipf(x, z, reduceByColSums = TRUE, eps = 0.1)
 #' max(abs(a1 - a2))
 #' max(abs(a1 - a3))
 #' 
 #' 
-#' # Improvement by reduceX. Changing eps and iter give more similar results.
+#' # Improvement by reduceByColSums. Changing eps and iter give more similar results.
 #' t <- 402
 #' y <- z3$ant
 #' y[round((1:t) * 432/t)] <- 0
 #' z <- t(x) %*% y
 #' a1 <- Mipf(x, z, eps = 1)
 #' a2 <- Mipf(x, z, reduceBy0 = TRUE, eps = 1)
-#' a3 <- Mipf(x, z, reduceX = TRUE, eps = 1)
+#' a3 <- Mipf(x, z, reduceByColSums = TRUE, eps = 1)
 #' max(abs(a1 - a2))
 #' max(abs(a1 - a3))
+#' 
+#' 
+#' # Improvement by ReduceByLeverage. Changing eps and iter give more similar results.
+#' t <- 378
+#' y <- z3$ant
+#' y[round((1:t) * 432/t)] <- 0
+#' z <- t(x) %*% y
+#' a1 <- Mipf(x, z, eps = 1)
+#' a2 <- Mipf(x, z, reduceBy0 = TRUE, eps = 1)
+#' a3 <- Mipf(x, z, reduceByColSums = TRUE, eps = 1)
+#' a4 <- Mipf(x, z, reduceByLeverage = TRUE, eps = 1)
+#' max(abs(a1 - a2))
+#' max(abs(a1 - a3))
+#' max(abs(a1 - a4))
+#' 
 #' 
 #' # Example with small eps and "Iteration stopped since tol reached"
 #' t <- 384
@@ -110,71 +167,68 @@
 #' z <- t(x) %*% y
 #' a1 <- Mipf(x, z, eps = 1e-14)
 #' a2 <- Mipf(x, z, reduceBy0 = TRUE, eps = 1e-14)
-#' a3 <- Mipf(x, z, reduceX = TRUE, eps = 1e-14)
+#' a3 <- Mipf(x, z, reduceByColSums = TRUE, eps = 1e-14)
 #' max(abs(a1 - a2))
 #' max(abs(a1 - a3))
 #' 
-#' # All y-data found by reduceX (0 iterations). 
+#' # All y-data found by reduceByColSums (0 iterations). 
 #' t <- 411
 #' y <- z3$ant
 #' y[round((1:t) * 432/t)] <- 0
 #' z <- t(x) %*% y
 #' a1 <- Mipf(x, z)
 #' a2 <- Mipf(x, z, reduceBy0 = TRUE)
-#' a3 <- Mipf(x, z, reduceX = TRUE)
+#' a3 <- Mipf(x, z, reduceByColSums = TRUE)
 #' max(abs(a1 - y))
 #' max(abs(a2 - y))
 #' max(abs(a3 - y))
-Mipf <- function(x, z, iter = 100, yStart = matrix(1, nrow(x), 1), eps = 0.01, tol = 1e-10, 
-                  reduceBy0 = FALSE, reduceX = FALSE, returnDetails = FALSE, y = NULL,
+Mipf <- function(x, z = NULL, iter = 100, yStart = matrix(1, nrow(x), 1), eps = 0.01, tol = 1e-10, 
+                  reduceBy0 = FALSE, reduceByColSums = FALSE, 
+                  reduceByLeverage = FALSE,
+                  returnDetails = FALSE, y = NULL,
+                  startCol = NULL,
                   ordSplit = FALSE, altSplit = FALSE) {
   
   if (is.null(z))
     z <- Matrix::crossprod(x, y)
   
-  if (reduceX) reduceBy0 <- TRUE
+  if (is.null(startCol)) {
+    xT <- as(drop0(x), "dgTMatrix")
+    startCol <- MatrixComponentSplit(xT = xT, altSplit = altSplit)
+  } else {
+    xT <- NULL
+  }
   
-  if (reduceBy0) {
-    cat("-")
-    snx <- seq_len(nrow(x))
-    flush.console()
-    a <- ReduceBy0(x, z, yStart)
-    aKnown <- as.vector(a$yKnown)
-    yHat <- Matrix(0, length(aKnown), 1)
-    yKnown <- aKnown
-    if (any(aKnown)) {
-      cat("0")
-      flush.console()
-    }
-    rerun <- reduceX
-    while (rerun) {
-      rerun <- FALSE
-      a <- ReduceXspes(a$x, a$z)
-      aKnown <- as.vector(a$yKnown)
-      if (any(aKnown)) {
-        cat("x")
-        flush.console()
-        yHat[(snx[!yKnown])[seq_along(aKnown)[aKnown]], 1] <- a$y[seq_along(aKnown)[aKnown], 1]
-        yKnown[!yKnown] <- aKnown
-        cat("-")
-        flush.console()
-        a <- ReduceBy0(a$x, a$z)
-        aKnown <- as.vector(a$yKnown)
-        rerun <- TRUE
-        if (any(aKnown)) {
-          cat("0")
-          flush.console()
-          yHat[(snx[!yKnown])[aKnown], 1] <- 0
-          yKnown[!yKnown] <- aKnown
-        }
-      }
-    }
+  
+  if (reduceByLeverage) reduceByColSums <- TRUE
+  
+  if (reduceByColSums) reduceBy0 <- TRUE
+  
+  if (reduceBy0)  {
     
-    cat("(",dim(x)[1],"*",dim(x)[2],"->", dim(a$x)[1],"*",dim(a$x)[2],")",sep="")
+    a <- Reduce0exact(x=x, z = z,  
+                                 reduceByColSums = reduceByColSums, 
+                                 reduceByLeverage = reduceByLeverage,
+                                 y = y, yStart = yStart,  printInc =TRUE) 
     
+    #return(a)
+    if(is.na(returnDetails))
+      return(a)
+    
+    yKnown <- a$yKnown
+    yHat <-a$y
+    
+    # Fix startCol after reduce 
+    sC <- rep(0, length(a$zSkipped))
+    sCseq <- seq_len(length(a$zSkipped))
+    for (i in seq_along(startCol)) 
+      sC[sCseq >= startCol[i]] <- i
+    sC <- sC[!a$zSkipped]
+    startCol <- which(!duplicated(sC))
+      
     if(any(!yKnown))
       yHat[seq_along(yKnown)[!yKnown], 1] <- Mipf(a$x, a$z, iter = iter, yStart = yStart[seq_along(yKnown)[!yKnown], 1], 
-                                                   eps = eps, tol = tol, ordSplit = ordSplit, altSplit = altSplit)
+                                                   eps = eps, tol = tol, ordSplit = ordSplit, altSplit = altSplit, startCol=startCol)
     else
       cat("   0 iterations\n")
     
@@ -194,67 +248,24 @@ Mipf <- function(x, z, iter = 100, yStart = matrix(1, nrow(x), 1), eps = 0.01, t
   cat(":")
   flush.console()
   
+  
+  
   # Split input into components
-  xT <- as(drop0(x), "dgTMatrix")
-  cat("-")
-  flush.console()
-  us <- UniqueSeq(xT@i, xT@j)
-  cat("-")
-  n <- max(us)
-  ma <- match(seq_len(n), us)
-  cat("-")
-  startCol <- c(xT@j[ma] + 1, ncol(x) + 1)
+  if (is.null(xT)) 
+    xT <- as(drop0(x), "dgTMatrix")
+  
+  startCol <- c(startCol, ncol(x) + 1)
+  
+  n <- length(startCol) - 1
+  
   xL <- vector("list", n)
   zL <- vector("list", n)
-  cat("-")
-  flush.console()
+    
   for (i in seq_len(n)) {
     xL[[i]] <- xT[, startCol[i]:(startCol[i + 1] - 1), drop = FALSE]
     zL[[i]] <- z[startCol[i]:(startCol[i + 1] - 1), 1, drop = FALSE]
   }
   
-  
-  
-  if (max(sapply(xL, function(x) max(rowSums(x)))) > 1) {
-    if (!altSplit) {
-      warning("Split into component not working properly. Try altSplit=TRUE ?")
-    } else {
-      warning("Alternative algorithm - since split into component not working properly")
-      
-      xL <- vector("list", 0)
-      zL <- vector("list", 0)
-      xU <- xT
-      
-      us <- UniqueSeq(xT@i, xT@j)
-      xU@x <- as.numeric(us)
-      
-      
-      m0 <- 0
-      ma <- 0
-      while (!is.na(ma)) {
-        ma <- match(2, xU@x)
-        if (is.na(ma)) {
-          mEnd <- ncol(xU)
-        } else {
-          mEnd <- min(xU@j[xU@x == 2])
-        }
-        xL1 <- xT[, m0 + (1:mEnd), drop = FALSE]
-        rS1 <- rowSums(xL1)
-        if (max(rS1) > 1) 
-          stop("something wrong")
-        if (!is.na(ma)) {
-          xU <- xU[, -(1:mEnd), drop = FALSE]
-          w1 <- which(rS1 == 1)
-          i1 <- xU@i %in% (w1 - 1)
-          xU@x[i1] <- xU@x[i1] - 1
-        }
-        xL <- c(xL, xL1)
-        zL <- c(zL, z[m0 + (1:mEnd), 1, drop = FALSE])
-        m0 <- m0 + mEnd
-      }
-      n <- length(xL)
-    }
-  }
   
   
   if (ordSplit) {
@@ -310,17 +321,64 @@ Mipf <- function(x, z, iter = 100, yStart = matrix(1, nrow(x), 1), eps = 0.01, t
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+MatrixComponentSplit <- function(x = NULL, xT = NULL, altSplit = FALSE, altSplitFORCE = FALSE) {
+  if (is.null(xT)) 
+    xT <- as(drop0(x), "dgTMatrix")
+  cat("-")
+  flush.console()
+  us <- UniqueSeq(xT@i, xT@j)
+  cat("-")
+  n <- max(us)
+  ma <- match(seq_len(n), us)
+  cat("-")
+  startCol <- c(xT@j[ma] + 1, ncol(xT) + 1)
+  cat("-")
+  flush.console()
+  
+  for (i in seq_len(n)) {
+    if (max(Matrix::rowSums(xT[, startCol[i]:(startCol[i + 1] - 1), drop = FALSE])) > 1) 
+      altSplitFORCE <- TRUE
+  }
+  
+  
+  if (altSplitFORCE) {
+    if (!altSplit) {
+      stop("Split into component not working properly. Try altSplit=TRUE ?")
+    } else {
+      warning("Alternative algorithm - since split into component not working properly")
+      
+      xU <- xT
+      
+      us <- UniqueSeq(xT@i, xT@j)
+      xU@x <- as.numeric(us)
+      
+      m0 <- 0
+      ma <- 0
+      
+      startCol <- 1
+      
+      while (!is.na(ma)) {
+        ma <- match(2, xU@x)
+        if (is.na(ma)) {
+          mEnd <- ncol(xU)
+        } else {
+          mEnd <- min(xU@j[xU@x == 2])
+        }
+        startCol <- c(startCol, m0 + mEnd + 1)
+        rS1 <- rowSums(xT[, m0 + (1:mEnd), drop = FALSE])
+        
+        if (max(rS1) > 1) 
+          stop("something wrong")
+        if (!is.na(ma)) {
+          xU <- xU[, -(1:mEnd), drop = FALSE]
+          w1 <- which(rS1 == 1)
+          i1 <- xU@i %in% (w1 - 1)
+          xU@x[i1] <- xU@x[i1] - 1
+        }
+        m0 <- m0 + mEnd
+      }
+    }
+  }
+  
+  return(startCol[-length(startCol)])
+}
